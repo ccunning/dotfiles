af41eece2957a1a4a1596404d9887572b7bd2e4a
Curt Cunning <curt.cunning@outlook.com>
2021-04-20 09:43:29 -0400

    change todo.txt aliases

--- .zshrc.d/gopath.zsh
+++
@@ -1,6 +0,0 @@
-# GoPath stuff
-export PATH="$PATH:${HOME}/.bin"
-export GOPATH="${HOME}/go"
-export GOBIN="${HOME}/go/bin/"
-export PATH=$PATH:$GOROOT/bin:$GOBIN
-export HISTCONTROL=ignorespace:ignoredups
